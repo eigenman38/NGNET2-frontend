405bf980f78e855670e6612c9f8c111fbde6fdb8
eigenman <eigenman38@gmail.com>
2022-01-19 15:14:17 -0700

    fixed proxy targets

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Inject, OnInit } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  private selector: string;
+
+  constructor(private elementRef: ElementRef) {
+    this.selector = elementRef.nativeElement.tagName;
+  }
+
+  ngOnInit(): void {
+    console.log(`ngOnInit: ${this.selector}`);
+  }
 }
